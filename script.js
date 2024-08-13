d7138fcdba2034331ce2e930d573d35196ed923a
Sergei-rgb1 <sergei25102007@bk.ru>
2024-08-13 17:40:33 +0400

    main

--- script.js
+++ script.js
@@ -10,5 +10,26 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false) {
-    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+    guess = prompt("Готовность, целься, огонь! (введите номер 0-6):");
+    if (guess < 0 || guess > 6) {
+        alert("Пожалуйста, введите действительный номер мобильного телефона!");
+    } else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess location3) {
+          hits = hits +1  
+        }
+    }
+}
+
+
+
+
+
+
+if (guess == location1) {
+    hits = hits + 1;
+} else if (guess == location2) {
+    hits = hits + 1;
+} else if (guess == location3) {
+    hits = hits + 1;
 }
