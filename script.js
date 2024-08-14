c9b12f9847cea52f3c96000257619416e346d245
Sergei-rgb1 <sergei25102007@bk.ru>
2024-08-14 17:59:43 +0400

    main

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 
 var guess;
 
@@ -10,25 +11,24 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false) {
-    guess = prompt("Готовность, целься, огонь! (введите номер 0-6):");
-    if (guess < 0 || guess > 6) {
-        alert("Пожалуйста, введите действительный номер мобильного телефона!");
+  guess = prompt("Готовность, целься, огонь! (введите номер 0-6):");
+  if (guess < 0 || guess > 6) {
+    alert("Пожалуйста, введите действительный номер мобильного телефона!");
+  } else {
+    guesses = guesses + 1;
+    if (guess == location1 || guess == location2 || guess == location3) {
+      hits = hits + 1;
+
+      if (hits == 3) {
+        isSunk = true;
+
+        alert("Ты потопил мой линкор!")
+      }
     } else {
-        guesses = guesses + 1;
-        if (guess == location1 || guess == location2 || guess == location3) {
-          hits = hits +1;  
-
-          if (hits == 3) {
-            isSunk = true;
-
-            alert("Ты потопил мой линкор!")
-          }
-          } else {
-            alert("MISS");
-        }
+      alert("Промах!");
     }
+  }
 }
-var stats = "Ты взял" + guesses + " догадывается потопить линкор, " +
-                    "а это значит, что ваша точность стрельбы была" + (3/guesses);
+var stats = "Ты решил" + guesses + " потопить линкор, " +
+  "и это значит, что твоя точность стрельбы на высоте!" + (3 / guesses);
 alert(stats);
- 
