a729624cb24abb4e4d96ea4c0d0022eeaf03bee9
Sergei-rgb1 <sergei25102007@bk.ru>
2024-08-14 22:36:50 +0400

    ain

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ while (isSunk == false) {
   } else {
     guesses = guesses + 1;
     if (guess == location1 || guess == location2 || guess == location3) {
+      alert("Попадание!");
       hits = hits + 1;
-
       if (hits == 3) {
         isSunk = true;
 
